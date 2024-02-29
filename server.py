5c832c912e1719f6e6ee6e08bc986334f27fc895
Tito Goyco <sinproper@gmail.com>
2024-02-29 10:00:02 -0500

    Remove unused import statement

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask_app import app
 from flask_app.controllers import users, jobs, shifts
-from flask_mail import Mail
+
 # from dotenv import load_dotenv
 # from flask_app.config.config import ss_client
 # from flask_app.controllers.webhooks_controller import webhooks_blueprint, setup_webhook
@@ -13,7 +13,7 @@ from flask_mail import Mail
 # setup_webhook(sheet_id='YOUR_SHEET_ID', callback_url='YOUR_CALLBACK_URL')
 
 # Assuming 'app' is your Flask app
-mail = Mail(app)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
